f7db2db6d544f34fb634210c3b0584e3ec13d0b4
Laura V Cardona <58284758+laura3212401957@users.noreply.github.com>
2026-05-12 03:30:38 +0000

    cambios

--- Proyecto/src/App.tsx
+++ Proyecto/src/App.tsx
@@ -18,7 +18,6 @@ function App() {
         <Link to="/informativa">Informativa</Link>
         <Link to="/usuarios">Usuarios</Link>
       </nav>
-
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/favorito' element={<Favorito/>} />
